f1db5817164e66fa47a1a62643080cd11529ddf3
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-06-22 19:17:07 +0200

    test iter

--- transformers/DropHighNaNColumnsTransformer.py
+++ transformers/DropHighNaNColumnsTransformer.py
@@ -7,14 +7,11 @@ class DropHighNaNColumnsTransformer(BaseEstimator, TransformerMixin):
         self.columns_to_drop = []
 
     def fit(self, X, y=None):
-        # Рассчитать долю пропусков в каждом столбце
         missing_ratios = X.isnull().mean()
-        # Определить столбцы для удаления
         self.columns_to_drop = missing_ratios[
             missing_ratios > self.threshold
         ].index.tolist()
         return self
 
     def transform(self, X, y=None):
-        # Удалить выбранные столбцы
         return X.drop(columns=self.columns_to_drop)
